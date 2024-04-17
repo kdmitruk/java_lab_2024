3177cd0f6efc00f9d646f49d42d2ad8ea34caa4d
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-04-17 12:56:38 +0200

    lab7z2

--- deaths/src/DeathCauseStatistics.java
+++ deaths/src/DeathCauseStatistics.java
@@ -8,6 +8,16 @@ public class DeathCauseStatistics {
     private int totalDeaths;
     private int[] stats;
 
+    public record AgeBracketDeaths(Integer young, Integer old, Integer deathCount){
+        @Override
+        public String toString() {
+            return "AgeBracketDeaths{" +
+                    "young=" + young +
+                    ", old=" + old +
+                    ", deathCount=" + deathCount +
+                    '}';
+        }
+    }
     public static DeathCauseStatistics fromCsvLine(String line) {
         Function<String, Integer> parseIntFromCsv = s -> s.equals("-") ? 0 : Integer.parseInt(s);
         DeathCauseStatistics result = new DeathCauseStatistics();
@@ -28,5 +38,14 @@ public class DeathCauseStatistics {
                 ", totalDeaths=" + totalDeaths +
                 ", stats=" + Arrays.toString(stats) +
                 '}';
+    }
+
+    public AgeBracketDeaths getDeathsForAge(Integer age){
+        int index = age >= 100 ? stats.length - 1 : age / 5;
+        return new AgeBracketDeaths(
+                index*5,
+                index == stats.length-1 ? null : index*5+4,
+                stats[index]
+        );
     }
 }
--- deaths/src/Main.java
+++ deaths/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         DeathCauseStatistics deathCauseStatistics = DeathCauseStatistics.fromCsvLine("A02.1          ,5,-,-,-,-,-,-,-,-,-,-,-,-,1,2,-,1,1,-,-,-");
-        System.out.println(deathCauseStatistics);
+        System.out.println(deathCauseStatistics.getDeathsForAge(66));
+
     }
 }
