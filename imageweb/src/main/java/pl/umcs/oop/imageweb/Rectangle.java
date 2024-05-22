88e0b3ddeec268adc198e30c980ca5894f8b6623
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-05-22 12:45:28 +0200

    lab11z2

---
+++ imageweb/src/main/java/pl/umcs/oop/imageweb/Rectangle.java
@@ -0,0 +1,5 @@
+package pl.umcs.oop.imageweb;
+
+public record Rectangle(int x, int y, int w, int h, String color) {
+
+}
--- imageweb/src/main/java/pl/umcs/oop/imageweb/RectangleController.java
+++ imageweb/src/main/java/pl/umcs/oop/imageweb/RectangleController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RectangleController {
-    @GetMapping
+
+    @GetMapping("hello")
     public String hello() {
         return "Hello world";
+    }
+
+    @GetMapping("rect")
+    public Rectangle rect() {
+        return new Rectangle(4, 2, 6, 9, "red");
     }
 
 }
