e5791ae1bc04fef50ef10b5df0487f2cfb72ad46
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-06-05 12:50:48 +0200

    lab13z1

--- gui_client/src/main/java/pl/umcs/oop/gui_client/HelloController.java
+++ gui_client/src/main/java/pl/umcs/oop/gui_client/HelloController.java
@@ -2,10 +2,22 @@ package pl.umcs.oop.gui_client;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 
 public class HelloController {
     @FXML
+    private TextField textField;
+    @FXML
+    private TextArea outputArea;
+
+
+    @FXML
     private void send() {
         System.out.println("SEND");
+        String message = textField.getText();
+        outputArea.appendText(message + "\n");
+        textField.clear();
     }
 }
