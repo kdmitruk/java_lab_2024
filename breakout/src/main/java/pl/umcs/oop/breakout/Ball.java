2a092d780c6e0aee2ea53902eb00b7ca7c7fe360
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-06-12 13:49:48 +0200

    lab14z3

---
+++ breakout/src/main/java/pl/umcs/oop/breakout/Ball.java
@@ -0,0 +1,27 @@
+package pl.umcs.oop.breakout;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Ball extends GraphicsItem{
+    Point2D moveVector = new Point2D(1, -1).normalize();
+
+    public Ball() {
+        width = 0.015 * canvasHeight;
+        height = 0.015 * canvasHeight;
+        x = 0.5 * canvasWidth;
+        y = 0.88 * canvasHeight;
+    }
+
+    @Override
+    public void draw(GraphicsContext graphicsContext) {
+        graphicsContext.setFill(Color.WHITE);
+        graphicsContext.fillOval(x, y, width, height);
+    }
+
+    public void updatePosition() {
+        x = x + moveVector.getX();
+        y = y + moveVector.getY();
+    }
+}
--- breakout/src/main/java/pl/umcs/oop/breakout/GameCanvas.java
+++ breakout/src/main/java/pl/umcs/oop/breakout/GameCanvas.java
@@ -9,19 +9,29 @@ import java.awt.*;
 public class GameCanvas extends Canvas {
     private GraphicsContext gc;
     private Paddle paddle;
+    private Ball ball;
+    private Boolean running = false;
     public void draw(){
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, 640, 700);
         paddle.draw(gc);
+        ball.draw(gc);
     }
 
     public void initialize(){
         gc = this.getGraphicsContext2D();
         GraphicsItem.setCanvasSize(getWidth(), getHeight());
         this.paddle = new Paddle();
+        ball = new Ball();
         this.setOnMouseMoved(mouseEvent -> {
             paddle.setX(mouseEvent.getX());
+            if(running) {
+                ball.updatePosition();
+            }
             draw();
+        });
+        this.setOnMouseClicked(mouseEvent -> {
+            running = true;
         });
         draw();
     }
